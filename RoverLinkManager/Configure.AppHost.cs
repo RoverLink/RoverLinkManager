a30dc17749676ecf857fe40e605787566367f172
Alexander Harrison <harrisona1aroverkidsorg@gmail.com>
2022-11-11 10:56:03 -0500

    Adding aws secrets before db init

--- RoverLinkManager/Configure.AppHost.cs
+++ RoverLinkManager/Configure.AppHost.cs
@@ -1,3 +1,5 @@
+using System.Text;
+using System.Text.Json;
 using Funq;
 using RoverLinkManager.Infrastructure.Secrets.AWS;
 using RoverLinkManager.Infrastructure.Secrets.AWS.Models;
@@ -12,6 +14,31 @@ namespace RoverLinkManager;
 public class AppHost : AppHostBase, IHostingStartup
 {
     public void Configure(IWebHostBuilder builder) => builder
+        .ConfigureAppConfiguration(builder =>
+        {
+            var config = builder.Build();
+
+            // Get the aws app secrets configuration from appsettings
+            var secretConfig = config.GetSection("AwsConfigurationSecrets").Get<SecretIdentifier>();
+
+            // Retrieve the secrets configuration from aws secrets
+            var settings = SecretsManager.GetSecret(secretConfig.Name, secretConfig.Region);
+
+            if (settings is null)
+            {
+                Log.Fatal("Unable to retrieve aws secrets for application settings");
+
+                throw new Exception("Unable to retrieve aws application settings.");
+            }
+
+            // Convert json to a stream
+            MemoryStream m = new MemoryStream();
+            m.Write(settings);
+            m.Flush();
+            m.Seek(0, SeekOrigin.Begin);
+
+            builder.AddJsonStream(m);
+        })
         .ConfigureServices(services => {
             // Configure ASP.NET Core IOC Dependencies
         });
@@ -25,19 +52,8 @@ public class AppHost : AppHostBase, IHostingStartup
             UseSameSiteCookies = true,
         });
 
-        // Get the aws app secrets configuration from appsettings
-        var secretConfig = AppSettings.Get<SecretIdentifier>("AwsConfigurationSecrets");
 
-        // Retrieve the secrets configuration from aws secrets
-        var settings = SecretsManager.GetSecret<Secrets>(secretConfig.Name, secretConfig.Region);
-
-        if (settings is null)
-        {
-            Log.Fatal("Unable to retrieve aws secrets for application settings");
-
-            throw new Exception("Unable to retrieve aws application settings.");
-        }
-
-        container.AddSingleton<Secrets>(settings ?? new());
+        //AppSettings.Set<Secrets>("Secrets", settings);
+        //container.AddSingleton<Secrets>(settings ?? new());
     }
 }
--- RoverLinkManager/Configure.Db.cs
+++ RoverLinkManager/Configure.Db.cs
@@ -1,6 +1,9 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using RoverLinkManager.Infrastructure.Secrets.AWS.Models;
 using ServiceStack;
+using ServiceStack.Admin;
+using ServiceStack.Configuration;
 using ServiceStack.Data;
 using ServiceStack.DataAnnotations;
 using ServiceStack.OrmLite;
@@ -12,7 +15,11 @@ namespace RoverLinkManager;
 public class ConfigureDb : IHostingStartup
 {
     public void Configure(IWebHostBuilder builder) => builder
-        .ConfigureServices((context, services) => {
+        .ConfigureServices((context, services) =>
+        {
+
+            var secrets = context.Configuration.Get<Secrets>();
+
             services.AddSingleton<IDbConnectionFactory>(new OrmLiteConnectionFactory(
                 context.Configuration.GetConnectionString("DefaultConnection")
                 ?? "Server=localhost;Database=test;User Id=test;Password=test;MultipleActiveResultSets=True;",
@@ -20,6 +27,7 @@ public class ConfigureDb : IHostingStartup
         })
         .ConfigureAppHost(appHost => {
             // Enable built-in Database Admin UI at /admin-ui/database
-            // appHost.Plugins.Add(new AdminDatabaseFeature());
+            appHost.Plugins.Add(new AdminDatabaseFeature());
+            appHost.Plugins.Add(new AdminUsersFeature());
         });
 }
