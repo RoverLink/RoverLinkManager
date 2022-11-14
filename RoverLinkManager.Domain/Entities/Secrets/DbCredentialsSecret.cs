20989ac51996d1dd2b6bc6ad5afda2e98c21eb82
Alexander Harrison <harrisona1aroverkidsorg@gmail.com>
2022-11-14 08:30:30 -0500

    Added EngineType field to properly recognize configuration strings

--- RoverLinkManager.Domain/Entities/Secrets/DbCredentialsSecret.cs
+++ RoverLinkManager.Domain/Entities/Secrets/DbCredentialsSecret.cs
@@ -1,9 +1,14 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
 
 namespace RoverLinkManager.Domain.Entities.Secrets;
 public enum DbType
 {
+    [EnumMember(Value = "unknown")]
+    Unknown,
+    [EnumMember(Value = "sqlserver")]
     SqlServer,
+    [EnumMember(Value = "postgres")]
     PostgreSql
 }
 
@@ -18,6 +23,34 @@ public class DbCredentialsSecret
     [JsonPropertyName("engine")]
     public string? Engine { get; set; }
 
+    public DbType EngineType
+    {
+        get
+        {
+            return Engine switch
+            {
+                "sqlserver" => DbType.SqlServer,
+                "postgres" => DbType.PostgreSql,
+                _ => DbType.Unknown
+            };
+        }
+        set
+        {
+            switch (value)
+            {
+                case DbType.SqlServer:
+                    Engine = "sqlserver";
+                    break;
+                case DbType.PostgreSql:
+                    Engine = "postgres";
+                    break;
+                default:
+                    Engine = "unknown";
+                    break;
+            }
+        }
+    }
+
     [JsonPropertyName("host")]
     public string? Host { get; set; }
 
@@ -30,13 +63,16 @@ public class DbCredentialsSecret
     [JsonPropertyName("database")]
     public string? Database { get; set; }
 
+    public bool Encrypt { get; set; } = true;
+    public int Timeout { get; set; } = 30;
+
     // PostgreSql connection string supported keywords = https://www.npgsql.org/doc/connection-string-parameters.html
     public string ToConnectionString()
     {
-        if (Engine == "sqlserver")
-            return $"Server={Host}, {Port};Database={Database};User Id={Username};Password={Password};Encrypt=true;Trusted_Connection=True;MultipleActiveResultSets=true;Timeout=30";
-        else if (Engine == "postgres")
-            return $"Host={Host};Port={Port};Database={Database};Username={Username};Password={Password};Timeout=30";
+        if (EngineType == DbType.SqlServer)
+            return $"Server={Host}, {Port};Database={Database};User Id={Username};Password={Password};Encrypt={Encrypt};Trusted_Connection=True;MultipleActiveResultSets=true;Timeout={Timeout}";
+        else if (EngineType == DbType.PostgreSql)
+            return $"Host={Host};Port={Port};Database={Database};Username={Username};Password={Password};Timeout={Timeout}";
         else return String.Empty;
     }
 }
